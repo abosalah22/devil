049805e157b57a71bb2925e2d014315c2963f74a
abosalah22 <90730a95@mohmal.com>
2016-09-05 12:05:50 +0300

    Create rebot.lua

---
+++ plugins/rebot.lua
@@ -0,0 +1,25 @@
+do 
+
+local function run(msg, matches) 
+
+if ( msg.text ) then 
+
+  if ( msg.to.type == "user" ) then 
+
+     return "للتحدث مع المطور ♔͡ مہٰٰٖہۣۚﹻهہٰٰٖنہۣۚﹻد ͡♔┇ℳr┇|┋ ادخل على المعرف 🌝🎈 @M_1_11          كروب الدعم الخاص بالبوت   🔕✔️  #      https://telegram.me/joinchat/B9DGez8Y7rj1jdeeCYAmkg" 
+  end 
+end 
+
+-- DEV @abo_shosho98 
+
+end 
+
+return { 
+  patterns = { 
+       "(.*)$" 
+  }, 
+  run = run, 
+} 
+
+end 
+-- BY @abo_shosho98
